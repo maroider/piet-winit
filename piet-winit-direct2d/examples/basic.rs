c95fec5bf578b5219a742c08b4858e4a34eb27d6
Markus Røyset <maroider@protonmail.com>
2020-01-12 15:43:39 +0100

    Get to a point where stuff kind of works

--- piet-winit-direct2d/examples/basic.rs
+++ piet-winit-direct2d/examples/basic.rs
@@ -1,3 +1,64 @@
+use direct2d::RenderTarget;
+use piet::RenderContext;
+use piet_direct2d::D2DRenderContext;
+use piet_test::draw_test_picture;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+use piet_winit_direct2d::create_render_target;
+
+// TODO: Improve error handling
+
+// TODO: Figure out how to make `Ctrl+C` not exit the application with `exit code: 0x80000003`
+
 fn main() {
-    //
+    let test_picture_number = std::env::args()
+        .skip(1)
+        .next()
+        .and_then(|s| s.parse::<usize>().ok())
+        .unwrap_or(0);
+
+    // Create the EventLoop and Window
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    // Create the D2D factory
+    let d2d = direct2d::factory::Factory::new().unwrap();
+    let dwrite = directwrite::factory::Factory::new().unwrap();
+
+    // Create the render target
+    let mut render_target = create_render_target(&window, &d2d);
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            Event::WindowEvent {
+                event: WindowEvent::Resized(_),
+                ..
+            } => {
+                // Create a new render target with the new dimensions for the window
+                render_target = create_render_target(&window, &d2d);
+            }
+            Event::RedrawRequested(_) => {
+                render_target.begin_draw();
+                let mut piet_context = D2DRenderContext::new(&d2d, &dwrite, &mut render_target);
+
+                // Draw stuff
+                draw_test_picture(&mut piet_context, test_picture_number).unwrap();
+
+                piet_context.finish().unwrap();
+                render_target.end_draw().unwrap();
+            }
+            _ => {}
+        }
+    });
 }
--- piet-winit-direct2d/src/lib.rs
+++ piet-winit-direct2d/src/lib.rs
@@ -1,1 +1,43 @@
+use std::mem;
 
+use direct2d::{
+    enums::{AlphaMode, RenderTargetType},
+    factory::Factory,
+    render_target::{GenericRenderTarget, HwndRenderTarget},
+};
+use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
+use winapi::{
+    shared::windef::{HWND, RECT},
+    um::winuser::GetClientRect,
+};
+use winit::window::Window;
+
+// TODO: Improve error handling
+
+/// Create a render target that covers the entire window
+///
+/// This function should be called again whenever the window's size chanes
+pub fn create_render_target(window: &Window, d2d: &Factory) -> GenericRenderTarget {
+    let hwnd = {
+        match window.raw_window_handle() {
+            RawWindowHandle::Windows(handle) => handle.hwnd as HWND,
+            _ => unreachable!(),
+        }
+    };
+
+    let mut rect: RECT = unsafe { mem::zeroed() };
+    if unsafe { GetClientRect(hwnd, &mut rect) } == 0 {
+        panic!("GetClientRect failed")
+    }
+    let width = (rect.right - rect.left) as u32;
+    let height = (rect.bottom - rect.top) as u32;
+
+    HwndRenderTarget::create(&d2d)
+        .with_hwnd(hwnd)
+        .with_target_type(RenderTargetType::Default)
+        .with_alpha_mode(AlphaMode::Unknown)
+        .with_pixel_size(width, height)
+        .build()
+        .unwrap()
+        .as_generic()
+}
