75acfc8755400d8d0824bd59ab576742e280cca4
Markus Røyset <maroider@protonmail.com>
2020-01-14 20:33:24 +0100

    Add antialiasing

--- piet-winit-direct2d/src/lib.rs
+++ piet-winit-direct2d/src/lib.rs
@@ -1,9 +1,9 @@
 use std::mem;
 
 use direct2d::{
-    enums::{AlphaMode, RenderTargetType},
+    enums::{AlphaMode, AntialiasMode, RenderTargetType},
     factory::Factory,
-    render_target::{GenericRenderTarget, HwndRenderTarget},
+    render_target::{GenericRenderTarget, HwndRenderTarget, RenderTarget},
 };
 use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
 use winapi::{
@@ -18,12 +18,7 @@ use winit::window::Window;
 ///
 /// This function should be called again whenever the window's size chanes
 pub fn create_render_target(window: &Window, d2d: &Factory) -> GenericRenderTarget {
-    let hwnd = {
-        match window.raw_window_handle() {
-            RawWindowHandle::Windows(handle) => handle.hwnd as HWND,
-            _ => unreachable!(),
-        }
-    };
+    let hwnd = hwnd_from_window(window);
 
     let mut rect: RECT = unsafe { mem::zeroed() };
     if unsafe { GetClientRect(hwnd, &mut rect) } == 0 {
@@ -32,12 +27,21 @@ pub fn create_render_target(window: &Window, d2d: &Factory) -> GenericRenderTarg
     let width = (rect.right - rect.left) as u32;
     let height = (rect.bottom - rect.top) as u32;
 
-    HwndRenderTarget::create(&d2d)
+    let mut render_target = HwndRenderTarget::create(&d2d)
         .with_hwnd(hwnd)
         .with_target_type(RenderTargetType::Default)
         .with_alpha_mode(AlphaMode::Premultiplied)
         .with_pixel_size(width, height)
         .build()
-        .unwrap()
-        .as_generic()
+        .unwrap();
+    render_target.set_antialias_mode(AntialiasMode::PerPrimitive);
+
+    render_target.as_generic()
+}
+
+fn hwnd_from_window(window: &Window) -> HWND {
+    match window.raw_window_handle() {
+        RawWindowHandle::Windows(handle) => handle.hwnd as HWND,
+        _ => unreachable!(),
+    }
 }
